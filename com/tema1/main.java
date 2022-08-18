c6bf157eb2b2a00df0f0f90bb85086529e799384
unknown <ihernanhergert@gmail.com>
2022-08-18 18:09:13 -0300

    reinitialized files

---
+++ com/tema1/main.java
@@ -0,0 +1,11 @@
+package com.tema1;
+
+public class main {
+    public static void main(String[] args) {
+        int numeroEntero = 0;
+        long numeroLong = 0;
+        double numeroDouble = 0;
+        boolean verdadero = true;
+        String cadena = "cadena";
+    }
+}
