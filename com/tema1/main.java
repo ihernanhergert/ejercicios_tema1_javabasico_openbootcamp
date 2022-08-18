073527c62fc47a3a4cf51132b969a323adb111f3
unknown <ihernanhergert@gmail.com>
2022-08-18 18:17:26 -0300

    first commit

--- com/tema1/main.java
+++ com/tema1/main.java
@@ -2,10 +2,17 @@ package com.tema1;
 
 public class main {
     public static void main(String[] args) {
-        int numeroEntero = 0;
-        long numeroLong = 0;
-        double numeroDouble = 0;
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 4;
+        long numero4 = 100;
+        float number5 = 10.5f;
+        double numeroDouble = 10.6564545342;
+        char caracter1 = 'h';
         boolean verdadero = true;
+        boolean falso = false;
+        Integer numero = null;
+        Long numero2 = 2L;
         String cadena = "cadena";
     }
 }
